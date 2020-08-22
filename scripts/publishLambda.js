461aa2be1532f425a60b46649914eac7b757eca6
Josh Menden <josh.menden@gmail.com>
2020-08-22 14:33:15 -0600

    Add support for AWS profile and region

--- scripts/publishLambda.js
+++ scripts/publishLambda.js
@@ -4,5 +4,11 @@ shell.cd('src')
 shell.exec('zip ../index.zip *')
 shell.cd('..')
 let commandString = `aws lambda create-function --function-name ${process.env.FUNCTION_NAME} --runtime nodejs12.x --role ${process.env.ROLE_ARN} --handler "index.handler" --zip-file "fileb://index.zip"`
+if (process.env.AWS_PROFILE) {
+  commandString += ` --profile ${process.env.AWS_PROFILE}`
+}
+if (process.env.AWS_REGION) {
+  commandString += ` --region ${process.env.AWS_REGION}`
+}
 shell.exec(commandString)
 shell.rm('index.zip')
--- scripts/updateLambda.js
+++ scripts/updateLambda.js
@@ -5,5 +5,11 @@ shell.cd('src')
 shell.exec('zip ../index.zip *')
 shell.cd('..')
 let commandString = `aws lambda update-function-code --function-name ${process.env.FUNCTION_NAME} --zip-file "fileb://index.zip"`
+if (process.env.AWS_PROFILE) {
+  commandString += ` --profile ${process.env.AWS_PROFILE}`
+}
+if (process.env.AWS_REGION) {
+  commandString += ` --region ${process.env.AWS_REGION}`
+}
 shell.exec(commandString)
 shell.rm('index.zip')
